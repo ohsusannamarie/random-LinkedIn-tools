49a872e0ca9a9dd4bffb75e575bb0b21a4114898
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-07-09 21:16:57 -0400

    Create Fixed_Name_Case.js

---
+++ Fixed_Name_Case.js
@@ -0,0 +1,23 @@
+/*/
+this will fix firstname lastname to be Firstname Lastname.
+This was built for those who are annoyed with undercase names
+works on LinkedIn, or LinkedIn Recruiter
+/*/
+
+var url = window.location.href;
+if(/linkedin\.com\/in/.test(url) === true){
+var nameElm = document.getElementsByClassName("pv-top-card-section__name")[0].innerHTML;
+document.getElementsByClassName("pv-top-card-section__name")[0].innerHTML = fixCase(nameElm);
+}
+if(/linkedin\.com\/recruiter/.test(url) === true){
+var nameElm = document.getElementsByClassName("profile-info")[0].getElementsByTagName("h1")[0].innerHTML;
+document.getElementsByClassName("profile-info")[0].getElementsByTagName("h1")[0].innerHTML = fixCase(nameElm);
+}
+
+function fixCase(str){
+return str.replace(
+        /\w\S*/g, function(az){
+            return az.charAt(0).toUpperCase() + az.substr(1).toLowerCase();
+        }
+    );
+}
