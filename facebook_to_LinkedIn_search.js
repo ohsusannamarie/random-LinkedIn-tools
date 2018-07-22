9295b608b00b00d818f835a4bc738bbc4f9499b9
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-07-22 18:41:38 -0400

    Create facebook_to_LinkedIn_search.js
    quick build to search for a person on LinkedIn by extracting their information from their facebook profile.

---
+++ facebook_to_LinkedIn_search.js
@@ -0,0 +1,19 @@
+function grouped(e, n){
+	if(e != null){ 
+		return e[n].toString();
+	}else{
+		return '';
+	}
+}
+var fullname = document.getElementById("fb-timeline-cover-name").innerText;
+var fn = 'firstName=' + grouped(/^([a-zA-Z]+\S*)\s/.exec(fullname), 1);
+var ln = '&lastName=' +/[a-zA-Z]+\S*[a-zA-Z]*$/.exec(fullname);
+
+if(/Works at/i.test(document.body.innerText)){	
+	var job = grouped(/Works at (.+)/i.exec(document.body.innerText), 1);
+	var co = '&company='+job;
+}else{
+	var co = '';
+}
+var startUrl = 'https://www.linkedin.com/search/results/people/?';
+window.open(startUrl+fn+ln+co);
