f001352366b9c4d09e5ec0a203ef1741b497ef48
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-07-22 19:39:30 -0400

    Update facebook_to_LinkedIn_search.js

--- facebook_to_LinkedIn_search.js
+++ facebook_to_LinkedIn_search.js
@@ -2,21 +2,25 @@
 watch the build at https://youtu.be/hjeuJrc-vrQ
 /*/
 function grouped(e, n){
-	if(e != null){ 
+	if(e != null){
 		return e[n].toString();
 	}else{
-		return '';
+		return ''; 
 	}
 }
+function boolOr(str){
+	return str.toString().replace(/,/g, ' OR '); 
+}
+var worker = document.getElementById("intro_container_id").getElementsByClassName("_3-90 _8o _8s lfloat _ohe img sp_8hfQa3kOwVD_2x sx_e8b967");
+var workArr = [];
+for(w=0; w<worker.length; w++){	
+	var job = grouped(/.+?at\s(.+)/ig.exec(worker[w].parentNode.innerText), 1);
+	workArr.push(job);
+}
+var co = '&company='+ boolOr(workArr).replace(/\s+/g, "%20")
 var fullname = document.getElementById("fb-timeline-cover-name").innerText;
 var fn = 'firstName=' + grouped(/^([a-zA-Z]+\S*)\s/.exec(fullname), 1);
 var ln = '&lastName=' +/[a-zA-Z]+\S*[a-zA-Z]*$/.exec(fullname);
 
-if(/Works at/i.test(document.body.innerText)){	
-	var job = grouped(/Works at (.+)/i.exec(document.body.innerText), 1);
-	var co = '&company='+job;
-}else{
-	var co = '';
-}
 var startUrl = 'https://www.linkedin.com/search/results/people/?';
 window.open(startUrl+fn+ln+co);
