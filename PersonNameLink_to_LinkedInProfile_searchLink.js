f23e4e5839b8ac1cb3ee54406f1b76e6ddef458c
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-08-01 09:21:27 -0400

    Update PersonNameLink_to_LinkedInProfile_searchLink.js

--- PersonNameLink_to_LinkedInProfile_searchLink.js
+++ PersonNameLink_to_LinkedInProfile_searchLink.js
@@ -7,27 +7,23 @@ function grouped(e, n){
 	}
 }
 
+var aychRef = document.getElementsByTagName("a");
 function clearLinks(){
-  this.parentElement.removeChild(document.getElementById("linkedyDobie"));
+	this.parentElement.removeChild(document.getElementById("linkedyDobie"));
 }
 
 function switcheroo(){
 	var regXProfName = /https:\/\/www\.facebook\.com\/\w.+?\w\?/;
 	if(regXProfName.test(this.href) === true){
-
-	var fn = grouped(/(^\w*\S+\w*)\s/.exec(this.innerHTML), 1);
-	var ln = grouped(/\s+(\w*\S+\w*)$/.exec(this.innerHTML), 1);
-  
-	var newlink = 'https://www.linkedin.com/search/results/people/?keywords=&firstName='+fn+'&lastName='+ln;
-  
-	this.href = newlink;
-	this.style.color = "green";
+		var fullnm = grouped(/(\w+.+?)(?:$|<)/.exec(this.innerHTML), 1);
+		var fn = grouped(/(^\w*\S+\w*)\s/.exec(fullnm), 1);
+		var ln = grouped(/\s+(\w*\S+\w*)$/.exec(fullnm), 1);
+		var newlink = 'https://www.linkedin.com/search/results/people/?keywords=&firstName='+fn+'&lastName='+ln;
+		this.href = newlink;
+		this.style.color = "green";
 	}
+
 }
-
-var aychRef = document.getElementsByTagName("a");
-
 Array.from(aychRef).forEach(function(element) {
    	element.addEventListener('mouseover', switcheroo);
     });
-
