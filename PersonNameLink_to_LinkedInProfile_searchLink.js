ec964e0f22559de1eeb4f8b8c64f50d9384d7b47
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-08-01 11:03:08 -0400

    Update PersonNameLink_to_LinkedInProfile_searchLink.js

--- PersonNameLink_to_LinkedInProfile_searchLink.js
+++ PersonNameLink_to_LinkedInProfile_searchLink.js
@@ -1,25 +1,31 @@
-
 function grouped(e, n){
-	if(e != null){
-		return e[n];
-	}else{
-		return '';
-	}
+  if (e != null) {
+    return e[n];
+  } else {
+    return '';
+  }
 }
 
 function switcheroo(){
-	var regXProfName = /https:\/\/www\.facebook\.com\/\w.+?\w\?/;
-	if(regXProfName.test(this.href) === true){
-		var fullnm = grouped(/(\w+.+?)(?:$|<)/.exec(this.innerHTML), 1);
-	var fn = grouped(/(^\w*\S+\w*)\s/.exec(fullnm), 1);
-	var ln = grouped(/\s+(\w*\S+\w*)$/.exec(fullnm), 1);
-	var newlink = 'https://www.linkedin.com/search/results/people/?keywords=&firstName='+fn+'&lastName='+ln;
-	this.href = newlink;
-	this.style.color = "green";
-	}
+  var regXProfName = /\w*\S+\w*/g;
+  var regXnumber = /\d|See More|replie|div /ig;
+  var fullnm = grouped(/(\w+.+?)(?:$|<)/.exec(this.innerHTML), 1);
+
+  var nameMatcher = fullnm.match(regXProfName);
+  if(nameMatcher != null & regXnumber.test(fullnm) === false){
+    if(nameMatcher.length > 1 && nameMatcher.length < 4) {
+      	var fn = grouped(/(^\w*\S+\w*)\s/.exec(fullnm), 1);
+      	var ln = grouped(/\s+(\w*\S+\w*)$/.exec(fullnm), 1);
+      	var newlink = 'https://www.linkedin.com/search/results/people/?keywords=&firstName=' + fn + '&lastName=' + ln;
+      	this.href = newlink;
+      	this.style.color = "green";
+	this.style.fontSize = "1.15em";
+	this.style.transition = "all 466ms ease-in-out";
+    }
+  }
 }
 var aychRef = document.getElementsByTagName("a");
 
-Array.from(aychRef).forEach(function(element) {
-   	element.addEventListener('mouseover', switcheroo);
-    });
+Array.from(aychRef).forEach(element => {
+  element.addEventListener('mouseover', switcheroo);
+});
